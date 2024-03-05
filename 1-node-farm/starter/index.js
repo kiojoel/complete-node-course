0d959b9f4f325003af501522a5970b9334235ccb
Akinsanya Joel <akinsanyajoel82@gmail.com>
2024-03-05 12:01:18 +0100

    first commit

---
+++ 1-node-farm/starter/index.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+
+////////////////////////////////////////
+// FILES
+
+//Blocking, synchronous way
+const inputText = fs.readFileSync("./txt/input.txt", "utf-8");
+console.log(inputText);
+
+const outputText = `This is what we know about avocado: ${inputText}.\nCreated on ${Date.now()}`;
+fs.writeFileSync("./txt/output.txt", outputText);
+console.log("Done!");
+
+//Non-blocking, asynchronous way
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  if (err) return console.log("Error");
+  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    console.log(data2);
+    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+      console.log(data3);
+
+      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+        console.log("Your file has been written!");
+      });
+    });
+  });
+});
+
+console.log("read file");
