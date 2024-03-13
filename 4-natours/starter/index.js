7d029effb62c9e9ed95d418908483d9dceeb24a2
Akinsanya Joel <akinsanyajoel82@gmail.com>
2024-03-14 00:25:08 +0100

    middleware

--- 4-natours/starter/index.js
+++ 4-natours/starter/index.js
@@ -1,33 +1,43 @@
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
+// MIDDLEWARES
+
+app.use(morgan('dev'));
+
 app.use(express.json());
 
-/* app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello from the server!', app: 'Natours' });
+app.use((req, res, next) => {
+  console.log('Hello from the middleware');
+  next();
 });
 
-app.post('/', (req, res) => {
-  res.status(200).send('You just post to this URL');
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
 });
- */
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.get('/api/v1/tours', (req, res) => {
+// ROUTE HANDLER
+
+const getAllTours = (req, res) => {
+  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
+    requestedAT: req.requestTime,
     data: {
       tours: tours,
     },
   });
-});
+};
 
-app.get('/api/v1/tours/:id', (req, res) => {
+const getTour = (req, res) => {
   console.log(req.params);
   const id = req.params.id * 1;
   if (id > tours.length) {
@@ -44,9 +54,9 @@ app.get('/api/v1/tours/:id', (req, res) => {
       tour,
     },
   });
-});
+};
 
-app.post('/api/v1/tours', (req, res) => {
+const createTour = (req, res) => {
   /* console.log(req.body); */
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
@@ -64,9 +74,9 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
-});
+};
 
-app.patch('/api/v1/tours/:id', (req, res) => {
+const updateTour = (req, res) => {
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: 'fail',
@@ -79,8 +89,9 @@ app.patch('/api/v1/tours/:id', (req, res) => {
       tour: 'updated tour here',
     },
   });
-});
-app.delete('/api/v1/tours/:id', (req, res) => {
+};
+
+const deleteTour = (req, res) => {
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: 'fail',
@@ -91,8 +102,28 @@ app.delete('/api/v1/tours/:id', (req, res) => {
     status: 'success',
     data: null,
   });
-});
+};
 
+// ROUTES
+
+/* app.get('/api/v1/tours', getAllTours);
+app.get('/api/v1/tours/:id', getTour);
+
+app.post('/api/v1/tours', createTour);
+
+app.patch('/api/v1/tours/:id', updateTour);
+
+app.delete('/api/v1/tours/:id', deleteTour);*/
+
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
+
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
+
+// START SERVER
 app.listen(3000, () => {
   console.log('App running on port 3000');
 });
